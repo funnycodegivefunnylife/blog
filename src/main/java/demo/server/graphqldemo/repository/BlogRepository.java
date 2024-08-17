5dd0186faa3a184058109f8f3adcd7a367c38905
datbht <datbht.backenddev@gmail.com>
2024-08-17 22:30:56 +0700

    feat: graphql

--- src/main/java/demo/server/graphqldemo/repository/BlogRepository.java
+++ src/main/java/demo/server/graphqldemo/repository/BlogRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BlogRepository extends JpaRepository<Blog, Long> {
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
     List<Blog> findAllByUserId(Integer userId);
 }
--- src/main/java/demo/server/graphqldemo/resolver/MutationResolver.java
+++ src/main/java/demo/server/graphqldemo/resolver/MutationResolver.java
@@ -4,10 +4,13 @@ import demo.server.graphqldemo.repository.Blog;
 import demo.server.graphqldemo.repository.BlogRepository;
 import demo.server.graphqldemo.repository.User;
 import demo.server.graphqldemo.repository.UserRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 @Component
+@Controller
 public class MutationResolver {
 
     private final UserRepository userRepository;
@@ -19,7 +22,7 @@ public class MutationResolver {
     }
 
     @MutationMapping
-    public User createUser(String username, String email, String passwordHash) {
+    public User createUser(@Argument String username, @Argument String email, @Argument String passwordHash) {
         User user = new User();
         user.setUsername(username);
         user.setEmail(email);
@@ -28,7 +31,7 @@ public class MutationResolver {
     }
 
     @MutationMapping
-    public Blog createBlog(String title, String content, Integer userId) {
+    public Blog createBlog(@Argument String title, @Argument String content, @Argument Integer userId) {
         Blog blog = new Blog();
         blog.setTitle(title);
         blog.setContent(content);
